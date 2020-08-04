5026d36f79f4df38f917925fd45742025e0722b5
LunarColony <yunisfarah4@gmail.com>
2020-08-04 17:13:00 +0100

    Initial Commit

---
+++ SimpleToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleToDoList
+//
+//  Created by Yunis Farah on 04/08/2020.
+//  Copyright © 2020 LunarColony. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
